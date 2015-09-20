bb33438d4a54a60332a3322737287162360e984a
Shane <shane8641@live.com>
2015-09-19 21:05:13 -0400

    Initial commit

---
+++ movieSort.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Movie Sort</title>
+</head>
+<body>
+<table border="0" cellspacing="3" cellpadding="3" align="center">
+<tr>
+    <td><h2>Rating</h2></td>
+    <td><h2>Title</h2></td>
+</tr>
+<?php
+/**
+ * Shane Teall
+ * Movie Sort Script
+ * 9/19/2015
+ */
+// create movies array
+$movies = array(
+    'Blackhat' => 7,
+    'Edge of Tomorrow' => 6,
+    'Hackers' => 5,
+    'Mad Max' => 9,
+    'ExMachina' => 8,
+    'Source Code' => 4,
+);
+// display in original order
+?>
+</table>
+</body>
+</html>
